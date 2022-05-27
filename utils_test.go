acd73e5fce263b66363d232deeafff2ee894a48f
Shiming Zhang <wzshiming@foxmail.com>
2022-05-27 14:11:12 +0800

    Support modify status according to annotations

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,7 @@
 package fake_kubelet
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -22,4 +23,58 @@ func TestParallelTasks(t *testing.T) {
 		t.Fatalf("Tasks completed too quickly: %v", elapsed)
 	}
 	t.Logf("Tasks completed in %v", elapsed)
+}
+
+func Test_modifyStatusByAnnotations(t *testing.T) {
+	type args struct {
+		origin []byte
+		anno   map[string]string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{
+			args: args{
+				origin: []byte(`{"phase":"Running"}`),
+				anno: map[string]string{
+					"fake/status.phase": "Succeeded",
+				},
+			},
+			want: []byte(`{"phase":"Succeeded"}`),
+		},
+		{
+			args: args{
+				origin: []byte(`{}`),
+				anno: map[string]string{
+					"fake/status.phase": "Succeeded",
+				},
+			},
+			want: []byte(`{"phase":"Succeeded"}`),
+		},
+		{
+			args: args{
+				origin: []byte(`{"containerStatuses":[{}]}`),
+				anno: map[string]string{
+					"fake/status.phase":                     `"Running"`,
+					"fake/status.containerStatuses.0.ready": "true",
+				},
+			},
+			want: []byte(`{"containerStatuses":[{"ready":true}],"phase":"Running"}`),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := modifyStatusByAnnotations(tt.args.origin, tt.args.anno)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("modifyStatusByAnnotations() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("modifyStatusByAnnotations() got = %q, want %q", got, tt.want)
+			}
+		})
+	}
 }
